1a848037ca2a083e5fdcb0d5217622191f78d007
amefoDC <creditamefo@gmail.com>
2019-02-21 21:58:19 +0100

    welcome back

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,80 +1,74 @@
 const express = require("express");
 const bookRouter = express.Router();
 
-const books = [
-  {
-    title: "War and peace",
-    genre: "Historical Fiction",
-    author: "Lev Nikolayevich Tolstoy",
-    read: false
-  },
-  {
-    title: "Les Miserables",
-    genre: "Historical Fiction",
-    author: "Victor Hugo",
-    read: false
-  },
-  {
-    title: "War and peace",
-    genre: "Historical Fiction",
-    author: "Lev Nikolayevich Tolstoy",
-    read: false
-  },
-  {
-    title: "THe time Machine",
-    genre: "Science Fiction",
-    author: "Jules Veerne",
-    read: false
-  },
-  {
-    title: "The Dark World",
-    genre: "Fantasy",
-    author: "Henry Cutner",
-    read: false
-  },
-  {
-    title: "The wind in the Willows",
-    genre: "Fantay",
-    author: "Kenneth Grahame",
-    read: false
-  },
-  {
-    title: "Life on the Mission",
-    genre: "History",
-    author: "Mark Twain",
-    read: false
-  },
-  {
-    title: "Childhold",
-    genre: "Biography",
-    author: "Lev Nikolayevich Tolstoy",
-    read: false
-  }
-];
+function router(nav) {
+  const books = [
+    {
+      title: "War and peace",
+      genre: "Historical Fiction",
+      author: "Lev Nikolayevich Tolstoy",
+      read: false
+    },
+    {
+      title: "Les Miserables",
+      genre: "Historical Fiction",
+      author: "Victor Hugo",
+      read: false
+    },
+    {
+      title: "War and peace",
+      genre: "Historical Fiction",
+      author: "Lev Nikolayevich Tolstoy",
+      read: false
+    },
+    {
+      title: "THe time Machine",
+      genre: "Science Fiction",
+      author: "Jules Veerne",
+      read: false
+    },
+    {
+      title: "The Dark World",
+      genre: "Fantasy",
+      author: "Henry Cutner",
+      read: false
+    },
+    {
+      title: "The wind in the Willows",
+      genre: "Fantay",
+      author: "Kenneth Grahame",
+      read: false
+    },
+    {
+      title: "Life on the Mission",
+      genre: "History",
+      author: "Mark Twain",
+      read: false
+    },
+    {
+      title: "Childhold",
+      genre: "Biography",
+      author: "Lev Nikolayevich Tolstoy",
+      read: false
+    }
+  ];
 
-bookRouter.route("/").get((req, res) => {
-  res.render("bookListView", {
-    nav: [
-      { link: "/books", title: "Books" },
-      { link: "/authors", title: "Authors" }
-    ],
-    title: "library",
-    books
+  bookRouter.route("/").get((req, res) => {
+    res.render("bookListView", {
+      nav,
+      title: "library",
+      books
+    });
   });
-});
-
-bookRouter.route("/:id").get((req, res) => {
-  const { id } = req.params;
-  res.render("bookView", {
-    nav: [
-      { link: "/books", title: "Books" },
-      { link: "/authors", title: "Authors" }
-    ],
-    title: "Library",
-    book: books[id]
+  bookRouter.route("/:id").get((req, res) => {
+    const { id } = req.params;
+    res.render("bookView", {
+      nav,
+      title: "Library",
+      book: books[id]
+    });
   });
+  return bookRouter;
+}
 
-  res.send("Hello single books");
-});
-
-module.exports = bookRouter;
+module.exports = router; 
