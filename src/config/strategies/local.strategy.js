58d6c6c07283d164200aa566a27db91b104dbd71
amefoDC <creditamefo@gmail.com>
2019-03-01 09:53:59 +0100

    added signin page, auth and goodreadsAPI

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,19 +1,43 @@
 const passport = require("passport");
-const { strategy } = require("passport-local");
+const { Strategy } = require("passport-local");
+const { MongoClient } = require("mongodb");
+const debug = require("debug")("app:local.strategy");
 
 module.exports = function localStrategy() {
   passport.use(
-    new strategy(
+    new Strategy(
       {
         usernameField: "username",
         passwordField: "password"
       },
       (username, password, done) => {
-        const user = {
-          username,
-          password
-        };
-        done(null, user);
+        const url = "mongodb://localhost:27017";
+        const dbName = "bookrApp";
+
+        (async function mongo() {
+          let client;
+
+          try {
+            client = await MongoClient.connect(url);
+
+            debug("Conneted correctly to server");
+
+            const db = client.db(dbName);
+            const col = db.collection("users");
+
+            const user = await col.findOne({ username });
+
+            if (user.password === password) {
+              done(null, user);
+            } else {
+              done(null, false);
+            }
+          } catch (err) {
+            console.log(err.stack);
+          }
+          //close connection
+          client.close();
+        })();
       }
     )
   );
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,65 +1,15 @@
 const express = require("express");
-const { MongoClient, ObjectID } = require("mongodb");
-const debug = require("debug")("app:bookRoutes");
+const bookController = require("../controllers/bookController.js");
 
 const bookRouter = express.Router();
+const bookService = require("../services/goodreadsService");
 
 function router(nav) {
-  bookRouter.route("/").get((req, res) => {
-    const url = "mongodb://localhost:27017";
-    const dbName = "bookrApp";
+  const { getIndex, getById, middleware } = bookController(bookService, nav);
+  bookRouter.use(middleware);
+  bookRouter.route("/").get(getIndex);
 
-    (async function mongo() {
-      let client;
-      try {
-        client = await MongoClient.connect(url);
-        debug("Connectes correctly to server");
-
-        const db = client.db(dbName);
-
-        const col = await db.collection("books");
-
-        const books = await col.find().toArray();
-
-        res.render("bookListView", {
-          nav,
-          title: "library",
-          books
-        });
-      } catch (err) {
-        debug(err.stack);
-      }
-      client.close();
-    })();
-  });
-
-  bookRouter.route("/:id").get((req, res) => {
-    const { id } = req.params;
-    const url = "mongodb://localhost:27017";
-    const dbName = "bookrApp";
-
-    (async function mongo() {
-      let client;
-      try {
-        client = await MongoClient.connect(url);
-        debug("Connectes correctly to server");
-
-        const db = client.db(dbName);
-
-        const col = await db.collection("books");
-        const book = await col.findOne({ _id: new ObjectID(id) });
-        debug(book);
-
-        res.render("bookView", {
-          nav,
-          title: "Library",
-          book
-        });
-      } catch (err) {
-        debug(err.stack);
-      }
-    })();
-  });
+  bookRouter.route("/:id").get(getById);
   return bookRouter;
 }
 
